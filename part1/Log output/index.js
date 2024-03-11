f6c35fcddfeb3abb6634d7c3cd204082df9b687a
rliu6915 <rliu6915@connect.hku.hk>
2024-03-11 15:39:02 +0800

    Exercise 1.07

--- part1/Log output/index.js
+++ part1/Log output/index.js
@@ -4,25 +4,11 @@ const Koa = require('koa')
 const app = new Koa()
 const PORT = process.env.PORT || 3000
 
-const genRandomString = () => {
-  const randomString = crypto.randomUUID()
-  // const newDate = new Date()
-
-  console.log(`${new Date()}: ${randomString}`)
-
-  setTimeout(genRandomString, 5000)
-}
-
-//const startingString = genRandomString()
-
 app.use(async ctx => {
   if (ctx.path.includes('favicon.ico')) return
 
-  const stringNow = genRandomString()
-  console.log('--------------------')
-  console.log(`Responding with ${stringNow}`)
-  ctx.body = `${startingString}: ${stringNow}`
+  const randomString = crypto.randomUUID()
+  ctx.body = `${new Date()}: ${randomString}`
 });
 
-//console.log(`Started with ${startingString}`)
 app.listen(PORT)
